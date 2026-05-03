2f8f39befec5cd1179e70976b7916cb7984da994
Furkenk <furkenk1@gmail.com>
2026-05-04 02:48:23 +0300

    Delete add-layers.js

--- add-layers.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-// This is a little script to replace translations.ts with the new languages.
-// We will just read the current file to get structure and then write back.
-// But to save time, since we can't easily parse and translate in Node without
-// an API, I will just rewrite translations.ts entirely using a tool call.
